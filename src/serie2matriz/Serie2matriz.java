47e11df018158c4a48bc4ea9c216f4d271af5e2c
Paola Bolaños <paola.bolanos@outlook.es>
2024-07-31 08:13:32 -0600

    Repositorio

---
+++ src/serie2matriz/Serie2matriz.java
@@ -0,0 +1,30 @@
+
+package serie2matriz;
+
+import java.util.Scanner;
+
+public class Serie2matriz {
+
+  
+    public static void main(String[] args) {
+     int matriz [][]= {{10,20,30,40},{1,2,3,4},{20,21,22,23}};
+        for (int i = 0; i <=matriz.length; i++) {
+            for (int j = 0; j <matriz[0].length; j++) {
+                
+            
+  
+        
+        System.out.println("Los datos de la matriz son: "+ matriz[i][j] );
+     } 
+    }      
+    }
+}
+
+
+    
+       
+
+    
+    
+    
+
